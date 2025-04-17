b05db851d3be1cddc82a65af608722f375ffe732
dollarplanet <agusriyanto.artist@gmail.com>
2025-04-17 15:24:54 +0700

    change elder question to forum

--- src/features/daily-question-forward.ts
+++ src/features/daily-question-forward.ts
@@ -4,21 +4,15 @@ import { readFileSync } from "fs";
 
 export class DailyQuestionForward extends MessageCreateFeatureBase {
   private dailyQuestionChannelId = "1362320556162158603";
-  private elderChannelId = "1360874619661324368";
+  private elderChannelId = "1362338943005229137";
 
   public async action(data: OmitPartialGroupDMChannel<Message<boolean>>) {
     // Harus dari channel daily question
     if (data.channelId !== this.dailyQuestionChannelId) return;
 
-    // Get content
-    const content = `
-    **Hey Adventurer ‼️**
-    ${data.embeds[0].description}
-
-
-    ᴮᵉʳᵖᵉᵗᵘᵃˡᵃⁿᵍ ᵗᵃⁿᵖᵃ ˡᵉˡᵃʰ ᵐᵉⁿᵘʲᵘ ᵘʲᵘⁿᵍ ˡᵃⁿᵍⁱᵗ
-    `;
-    if (!content) return;
+    // Get description
+    const description = data.embeds[0].description;
+    if (!description) return;
 
     // Get elder channel
     const elderChannel = data.guild?.channels.cache.get(this.elderChannelId);
@@ -29,15 +23,17 @@ export class DailyQuestionForward extends MessageCreateFeatureBase {
 
     // Payload
     const payload: MessageCreateOptions = {
-      content: content,
       files: [
         decorImage
       ]
     }
 
     // Kirim pesan ke elder channel
-    if (elderChannel.isSendable()) {
-      await elderChannel.send(payload);
+    if (elderChannel.isThreadOnly()) {
+      await elderChannel.threads.create({
+        name: description,
+        message: payload
+      });
     }
   }
 
