6e80ce596e6593dfe8962588933692fcd9f1f791
dollarplanet <agusriyanto.artist@gmail.com>
2025-05-04 12:08:10 +0700

    seed

--- prisma/seed/active-feature-seed.ts
+++ prisma/seed/active-feature-seed.ts
@@ -2,7 +2,7 @@ import { prisma } from "../../src/singleton/prisma-singleton";
 import { featureEnum } from "@prisma/xxx-client"
 
 export async function activeFeatureSeed() {
-  for (const permission of Object.values(featureEnum)) {
+  Object.values(featureEnum).forEach(async permission => {
     await prisma.activeFeature.upsert({
       where: {
         name: permission
@@ -12,5 +12,5 @@ export async function activeFeatureSeed() {
         name: permission,
       }
     });
-  }
+  })
 }
--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,7 +1,9 @@
 import { activeFeatureSeed } from "./active-feature-seed";
+import { activeIntentSeed } from "./active-intent-seed";
 
 async function main() {
   await activeFeatureSeed();
+  await activeIntentSeed();
 }
 
 main();
