4fc4b84c3e015195facf5e4685bf336a89964e0a
dollarplanet <agusriyanto.artist@gmail.com>
2025-05-04 12:25:31 +0700

    static feature init

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,40 @@
 import dotenv from "dotenv";
 import { discord } from "./singleton/client-singleton";
 import { startCron } from "./cron/runner";
-import { prisma } from "./singleton/prisma-singleton";
-import { FeatureMap } from "./utils/feature-map";
+import { AddRoleOnMemberJoin } from "./feature/action/add-role-on-member-join";
+import { ChangeNicknameChannel } from "./feature/action/change-nickname-channel";
+import { DailyQuestionForward } from "./feature/action/daily-question-forward";
+import { DirectWelcomeMessage } from "./feature/action/direct-welcome-message";
+import { DynamicVoiceChannelState } from "./feature/action/dynamic-voice-channel-state";
+import { GoodbyMessageAndDm } from "./feature/action/goodby-message-and-dm";
+import { MediaOnlyForum } from "./feature/action/media-only-forum";
+import { ProvinceRoleInNickname } from "./feature/action/province-role-in-nickname";
+import { ProvinceRoleNicknameRemove } from "./feature/action/province-role-nickname-remove";
+import { SetStatus } from "./feature/action/set-status";
+import { WelcomeBannerForward } from "./feature/action/welcome-banner-forward";
 
 // Load environment variables
 dotenv.config();
 
-async function init() {
-  const activeFeatures = (await prisma.activeFeature.findMany({ select: { name: true } })).map(feature => feature.name);
-
-  activeFeatures.forEach(feature => {
-    const featureClass = FeatureMap.get(feature);
-    new featureClass();
-  })
+function featureInit() {
+  new AddRoleOnMemberJoin();
+  new ChangeNicknameChannel();
+  new DailyQuestionForward();
+  new DirectWelcomeMessage();
+  new DynamicVoiceChannelState();
+  new GoodbyMessageAndDm();
+  new MediaOnlyForum();
+  new ProvinceRoleInNickname();
+  new ProvinceRoleNicknameRemove();
+  new SetStatus();
+  new WelcomeBannerForward();
 }
 
 // Register features
-init().then(async () => {
-  // Login
-  await discord().login(process.env.DISCORD_TOKEN)
-}).then(() => {
+featureInit();
+
+// Login
+discord().login(process.env.DISCORD_TOKEN).then(() => {
   // Start cron job
   startCron();
 });
