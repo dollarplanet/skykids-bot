ed48991308441c659ccc6280808876c5b51adbde
dollarplanet <agusriyanto.artist@gmail.com>
2025-05-04 12:22:06 +0700

    initial role on db

--- src/feature/action/add-role-on-member-join.ts
+++ src/feature/action/add-role-on-member-join.ts
@@ -1,11 +1,15 @@
 import { GuildMember } from "discord.js";
 import { GuildMemberAddListener } from "../base/guild-member-add-listener";
+import { prisma } from "../../singleton/prisma-singleton";
 
 export class AddRoleOnMemberJoin extends GuildMemberAddListener {
   public async action(member: GuildMember) {
     try {
-      // Add role adventurer
-      await member.roles.add("1360285795159642242");
+      // Get roles
+      const roles = await prisma.initialRole.findMany();
+
+      // Add roles
+      await member.roles.add(roles.map(role => role.roleId));
     } catch {
       //
     }
