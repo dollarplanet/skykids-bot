f2f17c9783bc5d015b7dc6f804e036e7b634add8
dollarplanet <agusriyanto.artist@gmail.com>
2025-05-10 01:00:07 +0700

    fish seed

---
+++ prisma/seed/fish-seed.ts
@@ -0,0 +1,36 @@
+import { prisma } from '../../src/singleton/prisma-singleton'
+
+export async function fishSeed() {
+  const fishJson = await import('./fish.json', {
+    assert: {
+      type: 'json'
+    }
+  })
+  const fishList = fishJson.default
+
+  for (const fish of fishList) {
+    await prisma.fish.upsert({
+      where: {
+        id: fish.id
+      },
+      update: {
+        name: fish.name,
+        image: fish.image,
+        rarity: fish.rarity,
+        price: fish.price,
+        time: fish.time,
+        months: fish.months,
+        updateAt: new Date()
+      },
+      create: {
+        id: fish.id,
+        name: fish.name,
+        image: fish.image,
+        rarity: fish.rarity,
+        price: fish.price,
+        time: fish.time,
+        months: fish.months
+      }
+    })
+  }
+}
--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,7 +1,9 @@
 import { activeFeatureSeed } from "./active-feature-seed";
+import { fishSeed } from "./fish-seed";
 
 async function main() {
   await activeFeatureSeed();
+  await fishSeed();
 }
 
 main();
